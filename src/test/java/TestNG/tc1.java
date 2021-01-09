057e584e6fbf897d8032de5e92aa9d095f3bcea7
meera k <33057346+meerasankarath@users.noreply.github.com>
2021-01-09 10:50:30 +0530

    Update tc1.java

--- src/test/java/TestNG/tc1.java
+++ src/test/java/TestNG/tc1.java
@@ -13,6 +13,13 @@ public class tc1 {
    		@BeforeMethod
 		public void launch() {
 		  System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		  ChromeOptions options = new ChromeOptions();
+		  options.addArguments("start-maximized"); // open Browser in maximized mode
+	          options.addArguments("disable-infobars"); // disabling infobars
+		  options.addArguments("--disable-extensions"); // disabling extensions
+		  options.addArguments("--disable-gpu"); // applicable to windows os only
+		  options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		  options.addArguments("--no-sandbox"); // Bypass OS security model
 		  driver = new ChromeDriver();
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
