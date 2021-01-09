d6ad05f4a46c7e7ed64a7284a4b6c9e564a82615
meera k <33057346+meerasankarath@users.noreply.github.com>
2021-01-09 15:09:27 +0530

    Update tc1.java

--- src/test/java/TestNG/tc1.java
+++ src/test/java/TestNG/tc1.java
@@ -26,7 +26,7 @@ public class tc1 {
 		  driver = new ChromeDriver(options);
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		  driver.get("http://15.207.89.196:32776/addressbook/");
+		  driver.get("http://13.232.208.208:32768/addressbook/");
 		
 		}
 		
